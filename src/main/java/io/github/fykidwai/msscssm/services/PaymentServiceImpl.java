2e53f7563a3a96644294009ffc8f88f33c84e232
Faiz Kidwai <f.y.kidwai@gmail.com>
2020-11-29 03:03:47 +0530

    adding init sm from db

--- src/main/java/io/github/fykidwai/msscssm/services/PaymentServiceImpl.java
+++ src/main/java/io/github/fykidwai/msscssm/services/PaymentServiceImpl.java
@@ -2,6 +2,7 @@ package io.github.fykidwai.msscssm.services;
 
 import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.config.StateMachineFactory;
+import org.springframework.statemachine.support.DefaultStateMachineContext;
 import org.springframework.stereotype.Service;
 
 import io.github.fykidwai.msscssm.domain.Payment;
@@ -18,27 +19,45 @@ public class PaymentServiceImpl implements PaymentService {
     private final StateMachineFactory<PaymentState, PaymentEvent> factory;
 
     @Override
-    public Payment newPayment(Payment payment) {
+    public Payment newPayment(final Payment payment) {
         payment.setPaymentState(PaymentState.NEW);
         return paymentRepository.save(payment);
     }
 
     @Override
-    public StateMachine<PaymentState, PaymentEvent> preAuth(Long paymentId) {
-        // TODO Auto-generated method stub
+    public StateMachine<PaymentState, PaymentEvent> preAuth(final Long paymentId) {
+        final StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
         return null;
     }
 
     @Override
-    public StateMachine<PaymentState, PaymentEvent> authorizePayment(Long paymentId) {
-        // TODO Auto-generated method stub
+    public StateMachine<PaymentState, PaymentEvent> authorizePayment(final Long paymentId) {
+        final StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
         return null;
     }
 
     @Override
-    public StateMachine<PaymentState, PaymentEvent> declineAuth(Long paymentId) {
-        // TODO Auto-generated method stub
+    public StateMachine<PaymentState, PaymentEvent> declineAuth(final Long paymentId) {
+        final StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
         return null;
+    }
+
+    private StateMachine<PaymentState, PaymentEvent> build(final Long paymentId) {
+        // Fetch Payment info from db
+        final Payment payment = paymentRepository.getOne(paymentId);
+
+        // Get StateMachine object
+        final StateMachine<PaymentState, PaymentEvent> sm = factory.getStateMachine(Long.toString(payment.getId()));
+
+        // Stop the state machine and reset so as to set only the state from the entity object
+        sm.stop();
+        sm.getStateMachineAccessor().doWithAllRegions(
+            sma -> sma.resetStateMachine(new DefaultStateMachineContext<>(payment.getPaymentState(), null, null, null)));
+
+        // Start the state machine and return
+        sm.start();
+
+        return sm;
     }
 
 }
