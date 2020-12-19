c6bd323abe5169965cb8697544d44aac563de229
Faiz Kidwai <f.y.kidwai@gmail.com>
2020-12-19 20:29:45 +0530

    Adding guards

--- src/main/java/io/github/fykidwai/msscssm/config/StateMachineConfig.java
+++ src/main/java/io/github/fykidwai/msscssm/config/StateMachineConfig.java
@@ -11,6 +11,7 @@ import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineConfigurationConfigurer;
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
 import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
+import org.springframework.statemachine.guard.Guard;
 import org.springframework.statemachine.listener.StateMachineListenerAdapter;
 import org.springframework.statemachine.state.State;
 
@@ -33,7 +34,7 @@ public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentSta
     @Override
     public void configure(final StateMachineTransitionConfigurer<PaymentState, PaymentEvent> transitions) throws Exception {
         transitions.withExternal().source(PaymentState.NEW).target(PaymentState.NEW).event(PaymentEvent.PRE_AUTHORIZE)
-            .action(preAuthAction())
+            .action(preAuthAction()).guard(paymentIdGuard())
 
             .and().withExternal().source(PaymentState.NEW).target(PaymentState.PRE_AUTH)
             .event(PaymentEvent.PRE_AUTH_APPROVED)
@@ -62,6 +63,10 @@ public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentSta
         };
 
         config.withConfiguration().listener(adapter);
+    }
+
+    private Guard<PaymentState, PaymentEvent> paymentIdGuard() {
+        return context -> context.getMessageHeader(PaymentServiceImpl.PAYMENT_ID_HEADER) != null;
     }
 
     private Action<PaymentState, PaymentEvent> preAuthAction() {
